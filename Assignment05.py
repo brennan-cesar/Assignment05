a5e506eade5102c1fcadf8e75075d09332eb4c76
Brennan <brennan.cesar@gmail.com>
2024-03-05 01:10:47 -0500

    added user input functionality

--- Assignment05.py
+++ Assignment05.py
@@ -47,18 +47,66 @@ class BasicMathOperations:
     
 
 def main():
+    looping = True
     firstName = input("What is your first name? >> ")
     lastName = input("What is your last name? >> ")
     user = BasicMathOperations(firstName, lastName)
 
-    print(user.sum(2,3))
-    print(user.operation(3,4,'*'))
-    print(user.square(3))
-    print(user.factorial(5))
-    print(user.count(14, 22))
-    print(user.calculateHypotenuse(2,3))
-    print(user.areaRectangle(6,8))
-    print(user.findPower(4,3))
-    print(user.typeCheck("hello!"))
+    while looping == True:
+        print("1: Greet user")
+        print("2: Add two numbers")
+        print("3: Perform operation on two numbers")
+        print("4: Square a number")
+        print("5: Compute a factorial")
+        print("6: Count from one number to another")
+        print("7: Calculate the hypotenuse of a right triangle")
+        print("8: Calculate the area of a rectangle")
+        print("9: Calculate the power of a number")
+        print("10: Get the type of an input")
+        print("0: exit program")
+        inp = int(input("What would you like to do? >> "))
+
+        if inp == 1:
+            print(f"Hello {user.firstName} {user.lastName}!")
+        elif inp == 2:
+            num1 = int(input("What is your first number? >> "))
+            num2 = int(input("What is your second number? >> "))
+            print(user.sum(num1, num2))
+        elif inp == 3:
+            num1 = int(input("What is your first number? >> "))
+            num2 = int(input("What is your second number? >> "))
+            op = input("What is the operator you wish to use? (+, -, *, /) >> ")
+            print(user.operation(num1, num2, op))
+        elif inp == 4:
+            num2 = int(input("What is your number? >> "))
+            print(user.square(num1))
+        elif inp == 5:
+            num2 = int(input("What is your number? >> "))
+            print(user.factorial(num1))
+        elif inp == 6:
+            num1 = int(input("What is your first number? >> "))
+            num2 = int(input("What is your second number? >> "))
+            print(user.count(num1, num2))
+        elif inp == 7:
+            num1 = int(input("What is the base? >> "))
+            num2 = int(input("What is the height? >> "))
+            print(user.calculateHypotenuse(num1, num2))
+        elif inp == 8:
+            num1 = int(input("What is the base? >> "))
+            num2 = int(input("What is the height? >> "))
+            print(user.areaRectangle(num1, num2))
+        elif inp == 9:
+            num1 = int(input("What is your first number? >> "))
+            num2 = int(input("What is your second number? >> "))
+            print(user.findPower(num1, num2))
+        elif inp == 10:
+            val = input("What is your variable to check? >> ")
+            print(user.typeCheck(val))
+        elif inp == 0:
+            print("Goodbye!")
+            looping == False
+            break
+
+
 
 main()
